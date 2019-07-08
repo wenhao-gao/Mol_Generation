813ebd7f570aab4a7f593808a1ea3c168d64c07c
mikegao95 <mikegao95@gmail.com>
2019-07-08 17:27:26 -0400

    Delete run_DQN.sh

--- run_DQN.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source activate rdkit
-TASK='test'
-HPARAMS='./configs/naive_dqn.json'
-CUDA_VISIBLE_DEVICES=0 nohup python \
-    -u train_dqn.py \
-    -t ${TASK} \
-    --hparams ${HPARAMS} &> ${TASK}.out &
